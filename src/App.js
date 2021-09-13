fdd8a0b9410fa30747b5c1329329b5196df97ab4
ryana <ryan.a.novelli@gmail.com>
2021-09-12 20:22:10 -0700

    Services Completed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Particles from "react-particles-js";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import About from "./components/About";
+import Services from "./components/Services";
 
 function App() {
   return (
@@ -68,6 +69,7 @@ function App() {
       <Navbar />
       <Header />
 	  <About />
+	  <Services />
     </>
   );
 }
