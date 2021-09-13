ed6d11a2f24dcfb6861083faf4a9be1bef833d2f
ryana <ryan.a.novelli@gmail.com>
2021-09-12 21:51:29 -0700

    Experience Completed

---
+++ src/components/Experience.js
@@ -0,0 +1,70 @@
+import React from "react";
+
+const Experience = () => {
+  return (
+    <div className="experience">
+      <div className="d-flex justify-content-center my-5">
+        <h1>experience</h1>
+      </div>
+      <div className="container experience-wrapper">
+        <div className="timeline-block timeline-block-right">
+          <div className="marker"></div>
+          <div className="timeline-content">
+            <h3>2018-2019</h3>
+            <p>
+              Veniam mollit voluptate fugiat magna incididunt laboris tempor
+              irure labore cillum veniam. Sint culpa reprehenderit non ullamco
+              cupidatat elit cupidatat nostrud. Fugiat velit officia ullamco
+              nisi nostrud ex ipsum ut in minim fugiat labore. Ipsum culpa
+              excepteur velit culpa.
+            </p>
+          </div>
+        </div>
+        {/* - */}
+        <div className="timeline-block timeline-block-left">
+          <div className="marker"></div>
+          <div className="timeline-content">
+            <h3>2019-2020</h3>
+            <p>
+              Veniam mollit voluptate fugiat magna incididunt laboris tempor
+              irure labore cillum veniam. Sint culpa reprehenderit non ullamco
+              cupidatat elit cupidatat nostrud. Fugiat velit officia ullamco
+              nisi nostrud ex ipsum ut in minim fugiat labore. Ipsum culpa
+              excepteur velit culpa.
+            </p>
+          </div>
+        </div>
+        {/* - */}
+        <div className="timeline-block timeline-block-right">
+          <div className="marker"></div>
+          <div className="timeline-content">
+            <h3>2020-2021</h3>
+            <p>
+              Veniam mollit voluptate fugiat magna incididunt laboris tempor
+              irure labore cillum veniam. Sint culpa reprehenderit non ullamco
+              cupidatat elit cupidatat nostrud. Fugiat velit officia ullamco
+              nisi nostrud ex ipsum ut in minim fugiat labore. Ipsum culpa
+              excepteur velit culpa.
+            </p>
+          </div>
+        </div>
+        {/* - */}
+        <div className="timeline-block timeline-block-left">
+          <div className="marker"></div>
+          <div className="timeline-content">
+            <h3>2021-2022</h3>
+            <p>
+              Veniam mollit voluptate fugiat magna incididunt laboris tempor
+              irure labore cillum veniam. Sint culpa reprehenderit non ullamco
+              cupidatat elit cupidatat nostrud. Fugiat velit officia ullamco
+              nisi nostrud ex ipsum ut in minim fugiat labore. Ipsum culpa
+              excepteur velit culpa.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Experience;
