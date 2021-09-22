ff853ccd456a5fcb8fc32b86c97c60b442f6c30c
ryana <ryan.a.novelli@gmail.com>
2021-09-21 17:35:29 -0700

    Footer Completed

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Experience from "./components/Experience";
 import Portfolio from "./components/Portfolio";
 import Testimonials from "./components/Testimonials";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -78,6 +79,7 @@ function App() {
 	  <Portfolio />
 	  <Testimonials />
     <Contact />
+    <Footer />
     </>
   );
 }
